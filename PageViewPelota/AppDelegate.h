60bef2866db17654489f7b3e703a32d8b3f72fcc
Sverovich <developer@serverasamac.local>
2017-01-21 10:15:08 -0600

    Initial Commit

---
+++ PageViewPelota/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PageViewPelota
+//
+//  Created by Sverovich on 1/21/17.
+//  Copyright © 2017 Asael Virgilio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PageViewPelota/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PageViewPelota
+//
+//  Created by Sverovich on 1/21/17.
+//  Copyright © 2017 Asael Virgilio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
